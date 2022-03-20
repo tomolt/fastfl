58009e3a94ce3b90c7518ab084243eacfca591f4
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2022-03-20 18:58:02 +0100

    Imported 20h's / Laslo's arg.h

--- src/fastfl.c
+++ src/fastfl.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <fastfl.h>
 
-int
-main()
+#include "arg.h"
+
+char *argv0;
+
+static void
+usage(void)
 {
+	fprintf(stderr, "usage: %s\n", argv0);
+	exit(1);
+}
+
+int
+main(int argc, char **argv)
+{
+	ARGBEGIN {
+	default:
+		usage();
+	} ARGEND
 	return 0;
 }
 
