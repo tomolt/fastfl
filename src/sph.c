cab09cd3fb62bb96b25a1f19cce5f1ee20e2e7ba
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2022-04-03 16:39:52 +0200

    SPH construction heuristic.

--- src/sph.c
+++ src/sph.c
@@ -1,13 +1,25 @@
 #include <stdlib.h>
+#include <math.h>
 
 #include "graph.h"
 
+struct condition {
+	bool  x_axis;
+	float threshold;
+};
+
+static inline bool
+eval_condition(const struct condition *cond, const FFL_Vertex *vert)
+{
+	return (cond->x_axis ? vert->x : vert->y) <= cond->threshold;
+}
+
 static int
-partition(FFL_Graph *graph, int low, int high)
+partition(FFL_Graph *graph, int low, int high, const struct condition *cond)
 {
 	for (;;) {
-		while (low < high && COND(graph->verts[low])) low++;
-		while (low < high && !COND(graph->verts[high-1])) high--;
+		while (low < high && eval_condition(cond, &graph->verts[low])) low++;
+		while (low < high && !eval_condition(cond, &graph->verts[high-1])) high--;
 		if (!(low < high)) break;
 		FFL_Vertex temp      = graph->verts[low];
 		graph->verts[low]    = graph->verts[high-1];
@@ -18,11 +30,31 @@ partition(FFL_Graph *graph, int low, int high)
 }
 
 static bool
-split_heuristic(FFL_Graph *graph, int low, int high)
+split_heuristic(const FFL_Graph *graph, int low, int high, struct condition *cond)
 {
-	(void) graph;
-	if (high - low <= 8) return true;
-	return false;
+	if (high - low <= 8) return false;
+
+	float min_x =  INFINITY, min_y =  INFINITY;
+	float max_x = -INFINITY, max_y = -INFINITY;
+
+	for (int i = 0; i < 6 || (min_x >= max_x && min_y >= max_y); i++) {
+		int v = rand() % graph->nverts;
+		const FFL_Vertex *vert = &graph->verts[v];
+		if (vert->x < min_x) min_x = vert->x;
+		if (vert->y < min_y) min_y = vert->y;
+		if (vert->x > max_x) max_x = vert->x;
+		if (vert->y > max_y) max_y = vert->y;
+	}
+
+	if (max_x - min_x >= max_y - min_y) {
+		cond->x_axis = true;
+		cond->threshold = 0.5f * (max_x + min_x);
+	} else {
+		cond->x_axis = false;
+		cond->threshold = 0.5f * (max_y + min_y);
+	}
+
+	return true;
 }
 
 static FFL_Clump *
@@ -30,7 +62,14 @@ build_clump(FFL_Graph *graph, int low, int high)
 {
 	FFL_Clump *clump = calloc(1, sizeof *clump);
 	clump->mass = high - low;
-	if (split_heuristic(graph, low, high)) {
+	struct condition cond;
+	if (split_heuristic(graph, low, high, &cond)) {
+		int border = partition(graph, low, high, &cond);
+		clump->nut = build_clump(graph, low, border);
+		clump->geb = build_clump(graph, border, high);
+		clump->sum_x = clump->nut->sum_x + clump->geb->sum_x;
+		clump->sum_y = clump->nut->sum_y + clump->geb->sum_y;
+	} else {
 		clump->is_leaf = true;
 		clump->low     = low;
 		clump->high    = high;
@@ -38,12 +77,6 @@ build_clump(FFL_Graph *graph, int low, int high)
 			clump->sum_x += graph->verts[i].x;
 			clump->sum_y += graph->verts[i].y;
 		}
-	} else {
-		int border = partition(graph, low, high);
-		clump->nut = build_clump(graph, low, border);
-		clump->geb = build_clump(graph, border, high);
-		clump->sum_x = clump->nut->sum_x + clump->geb->sum_x;
-		clump->sum_y = clump->nut->sum_y + clump->geb->sum_y;
 	}
 	return clump;
 }
